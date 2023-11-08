fc0bbbb047a5b0460bb0860f5fa68b11801bc91a
Cody Derbyshire <derbyshire@outlook.co.nz>
2023-11-08 15:41:27 +1300

    fixed random bullshit, still playing with  ui

--- src/Spotify.jsx
+++ src/Spotify.jsx
@@ -1,11 +1,8 @@
 import { useState, useEffect } from 'react';
 import React from 'react';
-import Play from './assets/play.png';
-import Pause from './assets/pause.png';
-import Next from './assets/forward.png';
-import Back from './assets/back.png';
 import './App.css';
 import axios from 'axios';
+import { motion } from 'framer-motion';
 
 const CLIENT_ID = '30f8bc7ee32a4537bdcd5666a008570e';
 const REDIRECT_URI = 'http://localhost:5173';
@@ -13,6 +10,49 @@ const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 const RESPONSE_TYPE = 'token';
 const SCOPE =
   'user-read-playback-state user-modify-playback-state user-read-currently-playing streaming user-library-read user-library-modify ugc-image-upload app-remote-control playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email user-read-private ';
+
+const icon = {
+  pause: (
+    <img
+      width='16'
+      height='16'
+      src='https://img.icons8.com/ios-glyphs/30/FFFFFF/pause--v1.png'
+      alt='pause--v1'
+    />
+  ),
+  play: (
+    <img
+      width='16'
+      height='16'
+      src='https://img.icons8.com/ios-glyphs/30/FFFFFF/play--v1.png'
+      alt='play--v1'
+    />
+  ),
+  next: (
+    <img
+      width='16'
+      height='16'
+      src='https://img.icons8.com/ios-glyphs/30/FFFFFF/end--v1.png'
+      alt='end--v1'
+    />
+  ),
+  back: (
+    <img
+      width='16'
+      height='16'
+      src='https://img.icons8.com/ios-glyphs/30/FFFFFF/skip-to-start--v1.png'
+      alt='skip-to-start--v1'
+    />
+  ),
+  exit: (
+    <img
+      width='30'
+      height='30'
+      src='https://img.icons8.com/ios-glyphs/30/FFFFFF/so-so.png'
+      alt='so-so'
+    />
+  ),
+};
 
 const App = () => {
   const [token, setToken] = useState('');
@@ -26,6 +66,8 @@ const App = () => {
   const [uri, setUri] = useState('');
   const [duration_ms, setDuration_ms] = useState('');
   const [isPlaying, setIsPlaying] = useState(false);
+  const [artist, setArtist] = useState([]);
+  const [artistId, setArtistId] = useState('');
 
   useEffect(() => {
     const hash = window.location.hash;
@@ -53,7 +95,7 @@ const App = () => {
 
     const fetchData = async () => {
       try {
-        const [userData, playbackData] = await Promise.all([
+        const [userData, playbackData, artistData] = await Promise.all([
           axios.get('https://api.spotify.com/v1/me', {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -68,6 +110,13 @@ const App = () => {
 
         setUser(userData.data);
         setPlayback(playbackData.data);
+
+        setProgress_ms(playback.progress_ms);
+        setDuration_ms(playback.item?.duration_ms);
+        setSong_name(playback.item?.name);
+        setArtist_name(playback.item?.artists?.[0]?.name);
+        setAlbum_name(playback.item?.album?.name);
+        setAlbum_art(playback.item?.album?.images?.[0]?.url);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -108,6 +157,8 @@ const App = () => {
   const logData = () => {
     console.log(playback);
     console.log(user);
+    console.log(token);
+    console.log(playback.item?.artists?.[0]?.id);
   };
 
   const pausePlayback = async () => {
@@ -117,15 +168,6 @@ const App = () => {
       headers: { Authorization: `Bearer ${token}` },
     });
     const pauseData = await data;
-    /* console.log(pauseData); */
-    console.log(playback.progress_ms);
-    console.log(playback.item?.duration_ms);
-    console.log(playback.item?.name);
-    console.log(playback.item?.artists?.[0]?.name);
-    console.log(playback.item?.album?.name);
-    console.log(playback.item?.album?.images?.[0]?.url);
-    console.log(playback.item?.uri);
-    console.log(playback.is_playing);
 
     setProgress_ms(playback.progress_ms);
     setDuration_ms(playback.item?.duration_ms);
@@ -188,52 +230,90 @@ const App = () => {
         </div>
       ) : (
         <div className='flex-row'>
+          {token && user ? (
+            <div className='flex-row'>
+              {user.images && user.images.length > 0 ? (
+                <img src={user.images?.[0].url} alt='' />
+              ) : null}
+            </div>
+          ) : null}
           <h1>reactify</h1>
-          <button className='' onClick={logout}>
-            logout
+          <button onClick={logout}>
+            <motion.div
+              initial={{ rotate: -360 }}
+              whileHover={{ rotate: 0 }}
+              transition={{ type: 'spring' }}
+            >
+              {icon.exit}
+            </motion.div>
           </button>
         </div>
       )}
-      {token && user ? (
-        <div className='flex-row'>
-          {user.images && user.images.length > 0 ? (
-            <img src={user.images?.[0].url} alt='' />
-          ) : null}
-          <p>{user.display_name}</p>
-        </div>
-      ) : null}
 
       {token && playback ? (
-        <p>
-          playing <strong>{playback.item.name}</strong> by
-          <strong> {playback.item.artists[0].name}</strong>
-        </p>
+        <div className='flex-column'>
+          <img
+            className='album-art'
+            src={playback.item?.album?.images?.[0]?.url}
+            alt='album artwork'
+          />
+
+          <div>
+            <p className='song-name'>{playback.item.name}</p>
+            <p className='artist-name'>{playback.item.artists[0].name}</p>
+            {/* <p className=''>{playback.item?.album?.name}</p> */}
+          </div>
+        </div>
       ) : null}
       <div className='flex-row'>
         {token && playback ? (
-          <button onClick={prevTrack}>
-            <img className='icon' src={Back} alt='' />
-          </button>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+            onClick={prevTrack}
+          >
+            {icon.back}
+          </motion.button>
         ) : null}
         {token && playback ? (
           <div>
             {playback.is_playing ? (
-              <button onClick={pausePlayback}>
-                <img className='icon' src={Pause} alt='' />
-              </button>
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+                onClick={pausePlayback}
+              >
+                {icon.pause}
+              </motion.button>
             ) : (
-              <button onClick={playPlayback}>
-                <img className='icon' src={Play} alt='' />
-              </button>
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+                onClick={playPlayback}
+              >
+                {icon.play}
+              </motion.button>
             )}
           </div>
         ) : null}
         {token && playback ? (
-          <button onClick={nextTrack}>
-            <img className='icon' src={Next} alt='' />
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+            onClick={nextTrack}
+          >
+            {icon.next}
+          </motion.button>
+        ) : null}
+        {user && playback ? (
+          <button className='log-data' onClick={logData}>
+            ?
           </button>
         ) : null}
-        {user && playback ? <button onClick={logData}>?</button> : null}
       </div>
     </div>
   );
